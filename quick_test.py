b0a51b217412920707d900b04594de5e31c450cc
clickok <babennet@ualberta.ca>
2014-10-29 02:23:59 -0600

    quick commit

--- quick_test.py
+++ quick_test.py
@@ -30,6 +30,13 @@ net = caffe.Classifier(MODEL_FILE, PRETRAINED,
 					   raw_scale=255,
 					   image_dims=(256, 256))
 
+# Load the classification labels
+labels = np.loadtxt(os.path.join(caffe_root, 'data/ilsvrc12/synset_words.txt'), str, delimiter='\t')
+
+# Get the labels that have 'bear' in them
+#bear_labels = [i for i, x in enumerate(labels) if 'bear' in x]
+bear_labels = [294, 295]
+
 # Set the phase to test
 net.set_phase_test()
 
@@ -38,3 +45,31 @@ net.set_mode_gpu()
 
 # Get the filenames of the images 
 image_paths = list(gen_find('./data/bearSightings', 'bear*.jpg'))
+
+# Try a single sample image
+input_image = caffe.io.load_image(image_paths[0])
+prediction = net.predict([input_image])
+
+# Get top predictions for image 
+top_pred = prediction.flatten().argsort()[::-1]
+
+# Now try it for all images...
+max_pred = 10 	# the number of top predictions to report
+for path in image_paths:
+	input_image = caffe.io.load_image(path)
+	prediction = net.predict([input_image])
+	top_pred = prediction.flatten().argsort()[::-1]
+	top_pred_lst = top_pred.tolist()
+
+	top_labels = labels[top_pred[:max_pred]]
+	print(top_labels)
+
+
+
+
+
+
+
+
+
+
